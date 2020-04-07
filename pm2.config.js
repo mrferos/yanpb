a16bd5e33c91e01ae3e2355cabefabf352030ccb
Andres Galindo <andres.galindo@flexshopper.com>
2020-04-06 21:26:09 -0400

    Fixed build folder

--- pm2.config.js
+++ pm2.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [{
         name        : "yanpb",
-        script      : "./__sapper__/builder/index.js",
+        script      : "./__sapper__/build/index.js",
         watch       : true,
         env: {
             "NODE_ENV": "development",
